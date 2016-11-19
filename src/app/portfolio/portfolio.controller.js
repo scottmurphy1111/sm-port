9e19452bcd962cb4025ff939df9afc5ea5c7e7b5
Scott Murphy <scott.murphy@martinagency.com>
2016-11-19 00:00:19 -0500

    center SO rep text, add comments to controller file

--- src/app/portfolio/portfolio.controller.js
+++ src/app/portfolio/portfolio.controller.js
@@ -1,39 +1,50 @@
 (function() {
   'use strict';
   
+  // fixes jshint error "Expected an assignment or function call and instead saw an expression."
   /* jshint expr: true */
 
   angular
     .module('website')
     .controller('PortfolioController', function($timeout, $state, $window, $document, $http, $scope) {
 
-      var portfolio = this;
+      var portfolio = this;//scope to portfolio
         
-      portfolio.loadMainContent = false;
+      portfolio.loadMainContent = false;//flag page load timer
 
+      //dynamic content animations and data loads
       portfolio.setUpPage = function() {
+        //trigger content load
         $timeout(function() {
           portfolio.loadMainContent = true;
         }, 800);
 
+        //add animation class to home section skills
         var toReveal = $document[0].querySelectorAll('.skills .to-reveal');
         
+        //ripple effect on skills reveal
         var processAll = function(els) {
           var timer = 0;
-            timer += 20*els+(Math.random()*5);
+          timer += 20*els+(Math.random()*5);
 
-            $timeout(function() {
-              toReveal[els].classList.add('show');
-            }, timer);
+          $timeout(function() {
+            toReveal[els].classList.add('show');
+          }, timer);
         };
 
+        $timeout(function() {
+          for (var n = 0; n < toReveal.length; n++) {
+            processAll(n);//closure
+          }
+        }, 3700);
+
+        //prevents bots from spamming my email/phone
         var loadContact = function() {
           var phone = '336-602-3121',
           phoneDiv = $document[0].querySelectorAll('.insert-phone'),
           user = 'scottmurphy1111',
           domain = 'gmail.com',
           emailDiv = $document[0].querySelectorAll('.insert-email');
-
 
           phoneDiv[0].innerHTML = '<a href="tel:' + phone + '">' + phone + '</a>';
           emailDiv[0].innerHTML = '<a href="mailto:' + user + '@' + domain + '">' + user + '@' + domain + '</a>';
@@ -42,12 +53,7 @@
 
         loadContact();
 
-        $timeout(function() {
-          for (var n = 0; n < toReveal.length; n++) {
-            processAll(n);
-          }
-        }, 3700);
-
+        //updates my experience year amount
         var setDate = $document[0].querySelectorAll('.experience-time'),
         calcDate = new Date().getFullYear() - 2009;
 
@@ -55,10 +61,7 @@
 
       };
 
-      portfolio.reloadPage = function() {
-        $window.location.reload();
-      };
-
+      //close project modal
       portfolio.closeModal = function() {
         $document[0].querySelectorAll('.modal-window')[0].classList.remove('show');
         $timeout(function() {
@@ -66,13 +69,13 @@
         }, 100);
       };
 
+      //set project jsfiddle var
       portfolio.chosenTemplate = "app/portfolio/modal-templates/creditwise.html";
 
       
-      //get SO rep number
+      //get Stack Overflow reputation count
       $scope.totalSo = 0;
       
-
       $http({
         method: 'GET',
         url: 'http://api.stackexchange.com/2.2/users/5711949/reputation?site=stackoverflow'
@@ -91,16 +94,15 @@
       }, function errorCallback(response) {
         throw new Error("Error" + response);
       });
-
-
-
     })
 
+    //right side vertical navigation
     .directive('vertNavItem', function($document) {
     
       var linkFunction = function(scope, element, attr) {
         element.bind('click', function(e) {
           e.stopPropagation();
+
           var clickedLiVal = attr.active,
           currentActiveLi = $document[0].querySelectorAll('.vert-nav li.active'),
           allPanels = $document[0].querySelectorAll('.sliding-panel'),
@@ -109,19 +111,21 @@
 
           activatePanel = $document[0].querySelectorAll('.'+clickedLiVal);
          
+          //loop through nav li's
           for(var i = 0; i < allPanels.length; i++) {
             allPanels[i].classList.add('fade-out');
-            allPanels[i].classList.remove('fade-in');
-              
+            allPanels[i].classList.remove('fade-in');              
           }
           
+          //update active li
           currentActiveLi[0].classList.remove('active');
           element[0].classList.add('active');
 
+          //show/hide clicked section
           activatePanel[0].classList.add('fade-in');
           activatePanel[0].classList.remove('fade-out');
           
-
+          //swipe icon hide/show
           if(clickedLiVal === 'top-panel') {
             enterBtn[0].classList.add('load-icon-instantly');
           } else {
@@ -137,9 +141,11 @@
         link: linkFunction
       };
     })
+
+    //scroll through sections
     .directive('onScroll', function($window, $document, $timeout) {
-        var isWheel = false;
-        var movingPanel = false;
+        var isWheel = false,//detect scroll wheel (PC)/swipe (Mac)
+        movingPanel = false;//detects if animation is in progress
 
         var linkFunction = function(scope, element) {
           $timeout(function() {
@@ -153,32 +159,41 @@
                   prevNav = $document[0].querySelectorAll('.vert-nav li.active')[0].previousElementSibling,
                   movement = 0;
               
+              //measure mousewheel/swipe distance
               movement = e.deltaY;
               
-              
-              
+              //firefox needs a little help to get correct distance
               if(e.deltaMode === 1) {
                 movement = movement * 8;
               }
-                           
+                     
+              //swap panels down     
               if(movement > 100 && nextNav) {
+                
+                //won't animate until false
                 if(isWheel) {  
                   return;
                 } 
                 
+                //allow animation
                 isWheel = true;
 
+                //mousewheel/swipe down
                 var movePanelDown = function() {
+
+                  //hide swipe icon
                   if(element[0].className.indexOf('top-panel') > -1) {
                     enterBtn[0].classList.remove('load-icon', 'load-icon-instantly');
                   }
 
+                  //show next panel/hide current panel
                   element.removeClass('load-content fade-in').addClass('fade-out');
                   nextPanel.addClass('fade-in').removeClass('fade-out');  
 
                   finishMovingDown();
                 };
 
+                //callback to prevent rapid animation
                 var finishMovingDown = function() {
                   if(movingPanel) {
                     return;
@@ -196,6 +211,7 @@
                 movePanelDown();
               }
 
+              //swap panels up
               if(movement < -100 && prevNav) {
                 if(isWheel) {  
                   return;
@@ -233,17 +249,14 @@
               }
             });
 
+            //mobile swiping swap panels
             var startX,
-                startY,
-                dist,
-                // threshold = 150, //required min distance traveled to be considered swipe
-                // allowedTime = 200, // maximum time allowed to travel that distance
-                // elapsedTime,
-                startTime;
+            startY,
+            dist,
+            startTime;
              
-
+            //inital touch
             element.bind('touchstart', function(e) {
-              //e.preventDefault();
               var touchobj = e.changedTouches[0];
 
               dist = 0,
@@ -253,12 +266,13 @@
               
             });
 
+            //prevent swipe end
             element.bind('touchmove', function(e){
                 e.preventDefault();
             });
          
+            //measure touch distance
             element.bind('touchend', function(e){
-              //e.preventDefault();
               var touchobj = e.changedTouches[0],
               dist = touchobj.pageY - startY,
               enterBtn = $document[0].querySelectorAll('.enter'),
@@ -268,25 +282,13 @@
               nextNav = $document[0].querySelectorAll('.vert-nav li.active')[0].nextElementSibling,
               prevNav = $document[0].querySelectorAll('.vert-nav li.active')[0].previousElementSibling;
 
-              if(dist < -10 && dist >= -50) {
-                           
-                // swipeIcon.classList.add('show');
-                // overlay.classList.add('show');
-              
-                // $timeout(function() {
-                //   swipeIcon.classList.remove('show');
-                //   overlay.classList.remove('show');
-                // }, 800);
-              }
-
+              //show/hide panel up
               if(dist < -100 && nextNav) {
                 if(isWheel) {  
                   return;
                 } 
                 
                 isWheel = true;
-                // swipeIcon.classList.remove('show');
-                // overlay.classList.remove('show');
 
                   var movePanelDown = function() {
                   if(element[0].className.indexOf('top-panel') > -1) {
@@ -316,6 +318,7 @@
                   movePanelDown();
               }
 
+              //show/hide panel down
               if(dist > 100 && prevNav) {
                 if(isWheel) {  
                   return;
@@ -360,27 +363,34 @@
           link: linkFunction
         };
     })
+
+    //third & fourth panels content reveals
     .directive('revealContent', function($interval, $document) {
     
       var linkFunction = function(scope, element) {
         element.bind('click', function() {
           var content = element.next(),
-          allBoxes = $document[0].querySelectorAll('.snippets li .content');
+          allContent = $document[0].querySelectorAll('.snippets li .content');
           
+          //hide content divs
           if(content.hasClass('show')){
-            for(var i = 0; i < allBoxes.length; i++) {
-              allBoxes[i].classList.remove('show');
+            for(var i = 0; i < allContent.length; i++) {
+              allContent[i].classList.remove('show');
             }
+
             content.removeClass('show');
-            //content.css('height', 0 +'px');
 
           } else {
-            for(var n = 0; n < allBoxes.length; n++) {
-              allBoxes[n].classList.remove('show');
+
+            //hide all revealed content
+            for(var n = 0; n < allContent.length; n++) {
+              allContent[n].classList.remove('show');
             }
+
+            //show clicked content
             content.addClass('show');
 
-
+            //reveal letters one at a time
             var contentText = $document[0].querySelectorAll('.content.show p')[0].textContent,
             updatePlace = $document[0].querySelectorAll('.content.show p')[0],
             current = 0,
@@ -394,11 +404,8 @@
                 updatePlace.innerHTML += contentText[current++];
               }
             }, 12);
-
           }
-          
         });
-      
       };
 
       return {
@@ -407,6 +414,8 @@
         link: linkFunction
       };
     })
+
+    //show modal window
     .directive('smModal', function($timeout, $document) {
     
       var linkFunction = function(scope, element) {
